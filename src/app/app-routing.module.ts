128cfca00844e5372c2daae78d201d04406e2879
shalineesingh <shalinee.singh@coviam.com>
2019-03-07 01:01:42 +0530

    links added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,63 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartsComponent } from './charts/charts.component';
+import { WorkflowDetailsComponent } from './charts/workflow-details/workflow-details.component';
+import { ApiTimelinesComponent } from './charts/api-timelines/api-timelines.component';
+import { RequestMapComponent } from './charts/request-map/request-map.component';
+import { CommunicationMapComponent } from './charts/communication-map/communication-map.component';
+import { TimeFilterComponent } from './time-filter/time-filter.component';
 
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+  {
+    path: 'home', component: DashboardComponent, data: {
+      breadcrumb: ''
+    }
+  },
+  {
+    path: 'details', component: ChartsComponent, data: {},
+    children: [
+      {
+        path: 'workflow', component: WorkflowDetailsComponent, data: {
+          breadcrumb: 'Workflow'
+        }
+      },
+      {
+        path: 'timeline', component: ApiTimelinesComponent, data: {
+          breadcrumb: 'ApiTimelinesComponent'
+        }
+      },
+      {
+        path: 'request', component: RequestMapComponent, data: {
+          breadcrumb: 'RequestMapComponent'
+        }
+      },
+      {
+        path: 'map', component: CommunicationMapComponent, data: {
+          breadcrumb: 'CommunicationMapComponent'
+        }
+      }
+    ]
+  }
+
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+
+export const routingComponents = [
+  DashboardComponent,
+  ChartsComponent,
+  WorkflowDetailsComponent,
+  ApiTimelinesComponent,
+  RequestMapComponent,
+  CommunicationMapComponent,
+  TimeFilterComponent
+];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ChartsComponent } from './charts/charts.component';
-import { WorkflowDetailsComponent } from './charts/workflow-details/workflow-details.component';
-import { ApiTimelinesComponent } from './charts/api-timelines/api-timelines.component';
-import { RequestMapComponent } from './charts/request-map/request-map.component';
-import { CommunicationMapComponent } from './charts/communication-map/communication-map.component';
-import { TimeFilterComponent } from './time-filter/time-filter.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    ChartsComponent,
-    WorkflowDetailsComponent,
-    ApiTimelinesComponent,
-    RequestMapComponent,
-    CommunicationMapComponent,
-    TimeFilterComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
